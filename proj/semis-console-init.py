3756a84029ac4a30f530b515f1364c3c73149489
thfruchart <66477650+thfruchart@users.noreply.github.com>
2024-12-10 17:24:06 +0100

    Create semis-console-init.py

---
+++ proj/semis-console-init.py
@@ -0,0 +1,14 @@
+# semis-console-init.py
+
+NB_CASES = 6
+plateau = [2]*NB_CASES
+joueur = +1 # le joueur 1 est représenté par +1
+# le joueur 2 est représenté par -1
+
+def tour_de_jeu(plateau, joueur, i):
+    """modifie le plateau en faisant jouer la case d'indice i  au joueur (+1 ou -1)
+    renvoie True si le coup est possible
+    et False si le coup est perdant"""
+    
+    return 
+
