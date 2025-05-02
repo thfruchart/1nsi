0f448c7acb61fe18ea2248c1d1c853e4729238cf
thfruchart <66477650+thfruchart@users.noreply.github.com>
2025-05-02 09:43:23 +0200

    Update fonction.js

--- S5/Exo9/fonction.js
+++ S5/Exo9/fonction.js
@@ -1,20 +1,7 @@
 function Couleur() 
 {
   
-  let rouge = Number(document.getElementById("rouge").value);
-  let hexaRouge = rouge.toString(16);
-  console.log(hexaRouge);
   
-  let vert = Number(document.getElementById("vert").value);
-  let hexaVert = vert.toString(16);
-  console.log(hexaVert);
+  console.log("PATIENCE");
   
-  let bleu = Number(document.getElementById("bleu").value);
-  let hexaBleu = bleu.toString(16);
-  console.log(hexaBleu);
-  
-  
-  let couleur = '#'+hexaRouge+hexaVert+hexaBleu;
-  console.log(couleur);
-  document.body.style.backgroundColor = couleur;
 }
