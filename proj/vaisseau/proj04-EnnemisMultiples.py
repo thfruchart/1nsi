899db0d00e0e3d81d5e36cf9356d75806f8925b4
thfruchart <66477650+thfruchart@users.noreply.github.com>
2026-03-27 15:19:05 +0100

    Add files via upload

---
+++ proj/vaisseau/proj04-EnnemisMultiples.py
@@ -0,0 +1,76 @@
+#####################################################
+# 1. créer une liste d'ennemis
+# 2. gérer l'apparition des ennemis au fur et à mesure
+#####################################################
+
+import pyxel
+import random 
+
+pyxel.init(256, 256, title="SPACE BATTLE")
+
+vaisseau = [60,60]
+DEPLACEMENT = 2
+
+tir_list = []
+VITESSE_TIR = 4
+
+ennemi_list = [ [240,128] ]
+
+
+
+# =========================================================
+# == UPDATE
+# =========================================================
+def update():
+    """mise à jour des variables (30 fois par seconde)"""
+    if pyxel.btn(pyxel.KEY_LEFT):
+        vaisseau[0] -= DEPLACEMENT
+    if pyxel.btn(pyxel.KEY_RIGHT):
+        vaisseau[0] += DEPLACEMENT
+    if pyxel.btn(pyxel.KEY_UP):
+        vaisseau[1] -= DEPLACEMENT
+    if pyxel.btn(pyxel.KEY_DOWN):
+        vaisseau[1] += DEPLACEMENT
+        
+    # TIR avec barre d'espace
+    if pyxel.btnp(pyxel.KEY_SPACE, 10,10):
+        tir =  [ vaisseau[0]+16 , vaisseau[1]+8 ]
+        tir_list.append(tir)
+    
+    # animation des tirs
+    for tir in tir_list : 
+        tir[0] += VITESSE_TIR
+    # création d'un ennemi
+    # déplacement des ennemis
+    for ennemi in ennemi_list : 
+        ennemi[0] -= 1
+    # détection des tirs
+    for tir in tir_list :
+        if exploser(ennemi,tir):
+            print('touché')
+    
+    
+
+
+# =========================================================
+# == DRAW
+# =========================================================
+def draw():
+    """création des objets (30 fois par seconde)"""
+    # vide la fenetre
+    pyxel.cls(0)
+    # vaisseau
+    pyxel.rect(vaisseau[0], vaisseau[1], 16, 16, 12)
+    # affichage ennemi
+    for ennemi in ennemi_list : 
+        pyxel.rect(ennemi[0], ennemi[1], 16, 16, 11)
+    # affichage TIRS
+    for tir in tir_list :
+        x1, y1 = tir[0], tir[1]
+        x2, y2 = x1 + 8 , y1
+        col = 10
+        pyxel.line(x1, y1, x2, y2, col)
+    
+    
+
+pyxel.run(update, draw)
